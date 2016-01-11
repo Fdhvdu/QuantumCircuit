98c97b86faf6613aff3cb9201f9c0458f92c9dc4
Fdhvdu <fdhvducipher@gmail.com>
2016-01-11 10:20:33 +0800

    add comment

--- header/CPath.h
+++ header/CPath.h
@@ -16,6 +16,24 @@ namespace nTsaiAlgorithm
 		void create_(std::size_t,std::size_t,std::size_t,T);
 	public:
 		CPath(T,T);
+		//0,7
+		//vec_={
+		//	{0,1,3,7},
+		//	{0,1,5,7},
+		//	{0,2,3,7},
+		//	{0,2,6,7},
+		//	{0,4,5,7},
+		//	{0,4,6,7}
+		//}
+		//7,0
+		//vec_={
+		//	{7,3,1,0},
+		//	{7,3,2,0},
+		//	{7,5,1,0},
+		//	{7,5,4,0},
+		//	{7,6,2,0},
+		//	{7,6,4,0}
+		//}
 		inline std::size_t diff_bit_count() const noexcept
 		{
 			return diff_bit_count_;
