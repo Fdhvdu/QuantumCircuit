d94564150b9a0a62bcbefafdd898720acc57aa34
Fdhvdu <fdhvducipher@gmail.com>
2016-01-15 13:29:02 +0800

    delete path_algorithm and route

--- header/path_algorithm.h
+++ header/path_algorithm.h
@@ -5,19 +5,11 @@
 namespace nQCircuit
 {
 	class CQGate;
-	class CQCircuit;
 }
 
 namespace nTsaiAlgorithm
 {
 	nQCircuit::CQGate createGate(size_t,size_t,size_t);
-
-	template<class RanIter>
-	nQCircuit::CQCircuit path_algorithm(RanIter,RanIter,size_t,size_t);	//when your bit is higher than 4, you have to modify this
-
-	std::size_t route(std::size_t);	//when your bit is higher than 4, you have to modify this
 }
-
-#include"path_algorithm.cpp"
 
 #endif
