5121fadfd79fe43d1444cce6e4eaf6e80dcd64ac
H.-K. Chen <fdhvducipher@gmail.com>
2016-06-15 16:37:38 +0800

    Update QCFwd.hpp

--- header/QCFwd.hpp
+++ header/QCFwd.hpp
@@ -16,8 +16,19 @@ class ICircuitAlgorithm
 {
 	Func_t func_;
 public:
+	//1. call algorithm
+	//2. create circuit"s" of the func_
 	virtual void create()=0;
+	//1. get the circuit"s"
+	//2. every nQCircuit::CQCircuit is an unique circuit
+	//3. to show the content of nQCircuit::CQCircuit, see nQCircuit::print or nQCircuit::print_t 
 	virtual const vec_CQCircuit& get() const noexcept=0;
+	//1.
+	//the func.size() must be 2^(the X-bit circuit you want to create)
+	//for example, if you are going to create a 3-bit circuit, the func.size() must be 8
+	//for example, if you are going to create a 4-bit circuit, the func.size() must be 16
+	//2. the value inside the func must be unique
+	//3. the range of value inside the func must be [0,func.size())
 	inline void setFunc(const Func_t &func)
 	{
 		func_=func;
